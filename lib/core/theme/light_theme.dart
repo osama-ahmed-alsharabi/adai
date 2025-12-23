4ae984678b458694a1aa1f5042163568ba162de9
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2025-12-23 16:00:49 +0300

    add the theme of the app

---
+++ lib/core/theme/light_theme.dart
@@ -0,0 +1,36 @@
+import 'package:adai/core/utils/app_color.dart';
+import 'package:flutter/material.dart';
+
+ThemeData light({Color color = AppColor.primaryColor}) => ThemeData(
+  // fontFamily: AppConst.fontFamily,
+  scaffoldBackgroundColor: Color(0xffF7F9FA),
+  primaryColor: color,
+  secondaryHeaderColor: AppColor.secondaryColor,
+  disabledColor: const Color(0xFFBABFC4),
+  brightness: Brightness.light,
+  hintColor: const Color(0xFF9F9F9F),
+  cardColor: Colors.white,
+  shadowColor: Colors.black,
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(foregroundColor: color),
+  ),
+  colorScheme: ColorScheme.light(primary: color, secondary: color)
+      .copyWith(surface: const Color(0xFFFCFCFC))
+      .copyWith(error: const Color(0xFFE84D4F)),
+  popupMenuTheme: const PopupMenuThemeData(
+    color: Colors.white,
+    surfaceTintColor: Colors.white,
+  ),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(500)),
+  ),
+  bottomAppBarTheme: const BottomAppBarTheme(
+    surfaceTintColor: Colors.white,
+    height: 60,
+    padding: EdgeInsets.symmetric(vertical: 5),
+  ),
+  dividerTheme: const DividerThemeData(
+    thickness: 0.2,
+    color: Color(0xFFA0A4A8),
+  ),
+);
