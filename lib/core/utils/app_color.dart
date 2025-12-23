0176eedd98172262e9f89250f83f35d50e3a434e
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2025-12-23 15:58:02 +0300

    adding the utils and create the app color

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const Color primaryColor = Color(0xff30355B);
+  static const Color secondaryColor = Color(0xff8E8CE0);
+  static const Color thirdColor = Color(0xff2BEDB3);
+}
