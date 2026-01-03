db4bf12acea25524d0ad2d7c0ace7f5a80406c26
osama alsharabi <osama.ahmed.alsharabii@gmail.com>
2026-01-04 02:17:17 +0300

    done with localization to make the app in arabic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:adai/core/router/app_router.dart';
 import 'package:adai/core/theme/light_theme.dart';
+import 'package:adai/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const Adai());
@@ -12,6 +14,14 @@ class Adai extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      locale: const Locale('ar'),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       debugShowCheckedModeBanner: false,
       theme: light(),
       routerConfig: AppRouter.router,
